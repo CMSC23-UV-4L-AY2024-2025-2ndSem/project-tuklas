02c46349818f5255afad73262397cf02a022755a
Aliyah Guoc <ajguoc@up.edu.ph>
2025-05-14 13:23:52 +0800

    add function to convert to map from json in buddy model

--- lib/models/travel_buddy_model.dart
+++ lib/models/travel_buddy_model.dart
@@ -3,4 +3,11 @@ class TravelBuddy {
   final String username;
 
   const TravelBuddy({required this.name, required this.username});
+
+  factory TravelBuddy.fromJson(Map<String, dynamic> json) {
+    return TravelBuddy(
+      name: json['name'],
+      username: json['username'],
+    );
+  }
 }
