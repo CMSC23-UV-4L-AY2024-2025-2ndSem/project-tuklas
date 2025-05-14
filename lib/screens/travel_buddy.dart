5106c86bc401f06fbf9b9241d41862f5c9472231
Aliyah Guoc <ajguoc@up.edu.ph>
2025-05-14 13:28:25 +0800

    added streambuilder in travel buddy page to track changes in db

--- lib/screens/travel_buddy.dart
+++ lib/screens/travel_buddy.dart
@@ -1,97 +1,117 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:project_TUKLAS/providers/user_profile_provider.dart';
 import '../models/travel_buddy_model.dart';
 
-class TravelBuddyScreen extends StatelessWidget {
-  const TravelBuddyScreen({super.key});
+class TravelBuddyScreen extends StatefulWidget {
 
-  final List<TravelBuddy> buddies = const [
-    TravelBuddy(name: "Aliyah Gabrielle", username: "@gab"),
-    TravelBuddy(name: "King Dela Cruz", username: "@king"),
-    TravelBuddy(name: "Paulene Aguilar", username: "@pau"),
-  ];
+  const TravelBuddyScreen({super.key});
+  
+  @override
+  State<TravelBuddyScreen> createState() => _TravelBuddyScreenState();
+}
+
+class _TravelBuddyScreenState extends State<TravelBuddyScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        "Good morning,",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Color.fromARGB(255, 0, 0, 0),
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+    return Scaffold( 
+      body: StreamBuilder(
+        stream: UserProfileProvider().getAllFriends(userId),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text("Error encountered! ${snapshot.error}"));
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (!snapshot.hasData) {
+            return const Center(child: Text("No Expenses Found"));
+          }
+
+        return SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Good morning,",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 4),
-                      Text(
-                        "Justin Peña",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFF14645B),
+                        SizedBox(height: 4),
+                        Text(
+                          "Justin Peña",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF14645B),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  ),
+                  const CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.grey,
+                    child: Icon(Icons.person, color: Colors.white),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.grey),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.grey),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 0,
+                    horizontal: 20,
                   ),
                 ),
-                const CircleAvatar(
-                  radius: 20,
-                  backgroundColor: Colors.grey,
-                  child: Icon(Icons.person, color: Colors.white),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Search',
-                prefixIcon: const Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: const BorderSide(color: Colors.grey),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: const BorderSide(color: Colors.grey),
-                ),
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 0,
-                  horizontal: 20,
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                "All Travel Buddies",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
               ),
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              "All Travel Buddies",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: snapshot.data?.docs.length,
+                  itemBuilder: (context, index) {
+                    TravelBuddy buddy = TravelBuddy.fromJson(
+                      snapshot.data?.docs[index].data() as Map<String, dynamic>,
+                    );
+                    return TravelBuddyItem(buddy: buddy);
+                  },
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ListView.builder(
-                itemCount: buddies.length,
-                itemBuilder: (context, index) {
-                  final buddy = buddies[index];
-                  return TravelBuddyItem(buddy: buddy);
-                },
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+      }
+      ));
   }
 }
 
@@ -102,6 +122,7 @@ class TravelBuddyItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 12.0),
       child: GestureDetector(
