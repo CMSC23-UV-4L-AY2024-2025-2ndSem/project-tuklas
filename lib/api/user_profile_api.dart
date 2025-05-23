7d2af5db7d5bbaa822e7dceb18ffb3fa33fd34a2
Justin Dayne Bryant Pena <jlpena1@up.edu.ph>
2025-05-23 23:54:59 +0800

    oks na to

--- lib/api/user_profile_api.dart
+++ lib/api/user_profile_api.dart
@@ -122,7 +122,6 @@ class FirebaseUserProfileApi {
     }
   }
 
-
   Future<String> updateUserProfileImage(
     String imageBase64,
     String username,
@@ -143,25 +142,37 @@ class FirebaseUserProfileApi {
   }
 
   // Create initial user profile
+  // In class FirebaseUserProfileApi
+
   Future<void> createUserProfile({
-    required String username,
+    required String
+    username, // Be cautious if this is intended to change the username set at auth sign-up
     required String firstName,
-    required String lastName, required bool isPublic, String? phoneNumber,
+    required String lastName,
+    required bool isPublic,
+    String? phoneNumber,
   }) async {
     final user = _auth.currentUser;
     if (user == null) {
-      throw Exception('No user logged in');
+      throw Exception('No user logged in for createUserProfile');
     }
 
     await _firestore.collection('users').doc(user.uid).set({
-      'username': username,
+      // 'username': username, // Only include if you intend to update username here.
+      // It's already set by FirebaseAuthAPI.signUp.
       'fname': firstName,
       'lname': lastName,
       'isPublic': isPublic,
       'phone': phoneNumber ?? '',
+      // These will re-initialize styles/interests if called.
+      // If styles/interests are set between signUp and this call, merge:true
+      // will preserve them only if these lines are removed or conditionally added.
+      // However, since FirebaseAuthAPI.signUp already initializes them,
+      // this re-initialization might be intended if this is a "reset profile" type function.
+      // For simply adding fname/lname, you might not want to touch styles/interests here.
       'styles': <String>[],
       'interests': <String>[],
-    });
+    }, SetOptions(merge: true)); // ✨ KEY CHANGE: Use merge: true
   }
 
   // Edit user styles
@@ -206,7 +217,7 @@ class FirebaseUserProfileApi {
   }
 
   // method to update user profile image base64 in Firestore
-  Future<String> updateProfileImage(String imageBase64,) async {
+  Future<String> updateProfileImage(String imageBase64) async {
     try {
       final user = _auth.currentUser;
       if (user == null) {
@@ -227,30 +238,33 @@ class FirebaseUserProfileApi {
     Map<String, dynamic> req = {
       'id': uid,
       'username': await findUsername(uid),
-      'name': await findName(uid)
+      'name': await findName(uid),
     };
     String? msg;
     try {
       String? buddyUser = await findId(buddyUid);
-      await FirebaseFirestore
-      .instance
-      .collection('users')
-      .doc(uid)
-      .collection('buddies')
-      .where('username', isEqualTo: buddyUser)
-      .limit(1)
-      .get()
-      .then((QuerySnapshot querySnapshot) async {
-        if (querySnapshot.docs.isEmpty) {
-          await _firestore.collection('users').doc(buddyUid).collection('requests').doc(uid).set(req);
-          msg = 'Successfully sent request!';
-        } else {
-          msg = 'Already friends with user!';
-        }
-        print (msg);
-        return msg;
-      });
-
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(uid)
+          .collection('buddies')
+          .where('username', isEqualTo: buddyUser)
+          .limit(1)
+          .get()
+          .then((QuerySnapshot querySnapshot) async {
+            if (querySnapshot.docs.isEmpty) {
+              await _firestore
+                  .collection('users')
+                  .doc(buddyUid)
+                  .collection('requests')
+                  .doc(uid)
+                  .set(req);
+              msg = 'Successfully sent request!';
+            } else {
+              msg = 'Already friends with user!';
+            }
+            print(msg);
+            return msg;
+          });
     } on FirebaseException catch (e) {
       msg = 'Error on ${e.code}: ${e.message}';
     }
@@ -262,19 +276,34 @@ class FirebaseUserProfileApi {
     final buddy = {
       'id': buddyUid,
       'username': await findUsername(buddyUid),
-      'name': await findName(buddyUid)
+      'name': await findName(buddyUid),
     };
     final userInf = {
       'id': user!.uid,
       'username': await findUsername(user.uid),
-      'name': await findName(user.uid)
+      'name': await findName(user.uid),
     };
     try {
-      if (accept){
-        await _firestore.collection('users').doc(buddyUid).collection('buddies').doc(user.uid).set(userInf);
-        await _firestore.collection('users').doc(user.uid).collection('buddies').doc(buddyUid).set(buddy);
+      if (accept) {
+        await _firestore
+            .collection('users')
+            .doc(buddyUid)
+            .collection('buddies')
+            .doc(user.uid)
+            .set(userInf);
+        await _firestore
+            .collection('users')
+            .doc(user.uid)
+            .collection('buddies')
+            .doc(buddyUid)
+            .set(buddy);
       }
-      await _firestore.collection('users').doc(user!.uid).collection('requests').doc(buddyUid).delete();
+      await _firestore
+          .collection('users')
+          .doc(user!.uid)
+          .collection('requests')
+          .doc(buddyUid)
+          .delete();
       print(userInf["username"]);
       print(userInf["name"]);
       print(buddy["username"]);
@@ -304,18 +333,21 @@ class FirebaseUserProfileApi {
   Future<String?> findName(String uid) async {
     String? name;
     await FirebaseFirestore
-      .instance // snapshot of db with usernames similar to username
-      .collection('users')
-      .where('id', isEqualTo: uid)
-      .limit(1)
-      .get()
-      .then((QuerySnapshot querySnapshot) {
-        if (querySnapshot.docs.isEmpty) {
-          name = null;
-        } else {
-          name = querySnapshot.docs[0]['fname'] + " " + querySnapshot.docs[0]['lname'];
-        }
-      });
+        .instance // snapshot of db with usernames similar to username
+        .collection('users')
+        .where('id', isEqualTo: uid)
+        .limit(1)
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+          if (querySnapshot.docs.isEmpty) {
+            name = null;
+          } else {
+            name =
+                querySnapshot.docs[0]['fname'] +
+                " " +
+                querySnapshot.docs[0]['lname'];
+          }
+        });
     return name;
   }
 
@@ -340,18 +372,18 @@ class FirebaseUserProfileApi {
   Future<String?> findUsername(String uid) async {
     String? username;
     await FirebaseFirestore
-      .instance // snapshot of db with usernames similar to username
-      .collection('users')
-      .where('id', isEqualTo: uid)
-      .limit(1)
-      .get()
-      .then((QuerySnapshot querySnapshot) {
-        if (querySnapshot.docs.isEmpty) {
-          username = null;
-        } else {
-          username = querySnapshot.docs[0]['username'];
-        }
-      });
+        .instance // snapshot of db with usernames similar to username
+        .collection('users')
+        .where('id', isEqualTo: uid)
+        .limit(1)
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+          if (querySnapshot.docs.isEmpty) {
+            username = null;
+          } else {
+            username = querySnapshot.docs[0]['username'];
+          }
+        });
     return username;
   }
 
